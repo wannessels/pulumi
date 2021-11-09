e20ffbebba605173afe02815cc573b650a39aa27
Fraser Waters <fraser@pulumi.com>
2021-11-09 16:01:23 +0000

    Dynamic provider test

---
+++ tests/integration/dynamic/dotnet/Program.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
+using Pulumi;
+
+//import binascii
+//import os
+//from pulumi import ComponentResource, export
+//from pulumi.dynamic import Resource, ResourceProvider, CreateResult//
+
+//class RandomResourceProvider(ResourceProvider):
+//    def create(self, props):
+//        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
+//        return CreateResult(val, { "val": val })//
+
+//class Random(Resource):
+//    val: str
+//    def __init__(self, name, opts = None):
+//        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)//
+
+//r = Random("foo")//
+
+//export("random_id", r.id)
+//export("random_val", r.val)
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => {});
+    }
+}
